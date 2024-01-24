a330bbe7eaf2ab37f4db1efc4e64675f5781e18e
HanbiHeo <hhb0365@gmail.com>
2024-01-24 16:11:59 +0900

    빌드그래들 수정, 웹소켓 일부

--- src/main/java/com/hopeback/config/WebSocketConfig.java
+++ src/main/java/com/hopeback/config/WebSocketConfig.java
@@ -1,4 +1,11 @@
 package com.hopeback.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
 public class WebSocketConfig {
 }
