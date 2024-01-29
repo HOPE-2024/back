b0e51254885e3115da59f589be1433fe635ed19c
HanbiHeo <hhb0365@gmail.com>
2024-01-30 08:51:04 +0900

    채팅 컨트롤러

--- src/main/java/com/hopeback/controller/ChatController.java
+++ src/main/java/com/hopeback/controller/ChatController.java
@@ -1,4 +1,28 @@
 package com.hopeback.controller;
 
+import com.hopeback.dto.chat.ChatRoomReqDto;
+import com.hopeback.dto.chat.ChatRoomResDto;
+import com.hopeback.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
 public class ChatController {
+    private final ChatService chatService;
+
+    //채팅방 생성
+    @PostMapping("/new")
+    public ResponseEntity<String> createRoom(@RequestBody ChatRoomReqDto chatRoomReqDto) {
+        ChatRoomResDto room = chatService.createRoom(chatRoomReqDto);
+        return ResponseEntity.ok(room.getRoomId());
+    }
+
 }
